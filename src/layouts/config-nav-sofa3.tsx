2a9f4452f79c6a1d890d1a7a86308c039accf47d
VinhCV042000 <caovinh@archway.co.jp>
2026-07-29 11:25:40 +0700

    Added config-nav-sofa3.tsx

---
+++ src/layouts/config-nav-sofa3.tsx
@@ -0,0 +1,34 @@
+import { paths } from 'src/routes/paths';
+
+import { Iconify } from 'src/components/iconify';
+
+// ----------------------------------------------------------------------
+
+export const sofa3NavData = [
+  { title: 'Trang chủ', path: '/sofa3', icon: <Iconify width={22} icon="solar:home-2-bold-duotone" /> },
+  {
+    title: 'Sản phẩm',
+    path: '/sofa3#products',
+    icon: <Iconify width={22} icon="solar:armchair-bold-duotone" />,
+  },
+  {
+    title: 'Vật liệu',
+    path: '/sofa3#materials',
+    icon: <Iconify width={22} icon="solar:leaf-bold-duotone" />,
+  },
+  {
+    title: 'Không gian',
+    path: '/sofa3#inspirations',
+    icon: <Iconify width={22} icon="solar:gallery-bold-duotone" />,
+  },
+  {
+    title: 'Đánh giá',
+    path: '/sofa3#testimonials',
+    icon: <Iconify width={22} icon="solar:star-bold-duotone" />,
+  },
+  {
+    title: 'Liên hệ',
+    path: paths.contact,
+    icon: <Iconify width={22} icon="solar:phone-bold-duotone" />,
+  },
+];
